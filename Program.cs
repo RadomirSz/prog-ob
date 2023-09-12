ca3348bcf8f3a0d2a46f712a7da0ffdf9a540d83
Radomir <114557710+RadomirSz@users.noreply.github.com>
2023-09-12 12:11:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Projecto1;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, World!");
+        System.Console.WriteLine("working?");
+    }
+}
