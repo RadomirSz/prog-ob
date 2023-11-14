854aa44577145a295a49876b63b0c0a83d8a4e09
RadomirSz <r.szpruta@gmail.com>
2023-11-14 10:07:35 +0100

    some cummit

---
+++ 6-14-11/Tablice-kontynuacja.cs
@@ -0,0 +1,15 @@
+﻿namespace _6_14_11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] name = {"Adam","Bartosz","Cezary","Damian","Kacper"};
+        foreach (string i in name) System.Console.Write(i + " ");
+        System.Console.WriteLine();
+        System.Console.WriteLine(name[2]);
+        System.Console.WriteLine(name[4]);
+
+        
+    }
+}
