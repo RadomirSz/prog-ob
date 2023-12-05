d6256504afc76bbad4048106bbdfff448ec0bf09
RadomirSz <r.szpruta@gmail.com>
2023-12-05 11:36:32 +0100

    jebfdfihweerbgfiuergh9

---
+++ 8-05-12/listy.cs
@@ -0,0 +1,54 @@
+﻿namespace _8_05_12;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region notatka
+        /*
+        //tworzenie listy
+        List<int> ints = new List<int>();
+        //inicjalizator kolekcji or some shit
+        List<int> ints1 = new List<int>{1,2,1,3,4,5,10,-2,1};
+        
+        //dodawanie elementów do listy za pomocą metody add
+        ints1.Add(1);
+        ints1.Add(21);
+
+        //usuwanie elementów z listy
+
+        ints1.Remove(1);
+
+        //wyswietlenie dlugości listy za pomocą właściwości Count
+        System.Console.WriteLine(ints1.Count);
+        foreach(int i in ints1) System.Console.WriteLine(i);
+        */
+        #endregion
+
+        #region zadanie
+        //utwórz listę liczb całkowitych i wypełnij ją losowymi wartościami z zakresu od 1 do 100.
+	    //Napisz metodę, która przyjmuje jako parametr listę liczb i zwraca listę liczb, które są podzielne przez 3 lub 5.
+	    //Wyświetl na konsoli listę liczb losowych i listę liczb podzielnych przez 3 lub 5.
+        List<int> L1 = new List<int>{1,3,9,4,6,10,10,8,9,5,4,23,45,7,8,4};
+        L1 = podmien(L1);
+        foreach(int i in L1) System.Console.WriteLine(i);
+        
+        System.Console.WriteLine("druga: ");
+
+        Random r = new Random();
+        List<int> L = new List<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            L.Add(r.Next(1,101));
+            System.Console.WriteLine(L[i]);
+        }
+        #endregion
+
+    }
+    static List<int> podmien(List<int> L)
+    {
+        List<int> L2 = new List<int>();
+        foreach (int i in L) if(i%3==0 || i%5==0) L2.Add(i);
+        return L2;
+    }
+}
---
+++ sprawdziany/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace sprawdziany;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //dopisz do każdej zmiennej "1"
+        int n1 = 5;
+        int[] tablica1 = UtworzTabliceLiczb(n1);
+        wyswietlTAB(tablica1);
+        
+        System.Console.WriteLine();
+        
+        int[] pola = new int[n1];
+        int[] obwody = new int[n1];
+
+        for (int i = 1; i < tablica1.GetLength(0)+1; i++)
+        {
+            int x = ObliczPole(tablica1[i-1]);
+            pola[i-1] = x;
+            System.Console.WriteLine("Pole kwadratu utworzonego przy użyciu liczby " + i + " z twojej tablicy to: " + x + "cm\u00B2");
+        }
+
+        System.Console.WriteLine();
+
+        for (int i = 1; i < tablica1.GetLength(0)+1; i++)
+        {
+            int x = ObliczObwod(tablica1[i-1]);
+            obwody[i-1] = x;
+            System.Console.WriteLine("Obwód kwadratu utworzonego przy użyciu liczby " + i + " z twojej tablicy to: " + x + "cm");
+        }
+
+       
+    }
+
+    static int dajcyfre()
+    {
+        System.Console.WriteLine("Podaj liczbę z zakresu 1-10:");
+        while(true)
+        {
+            if(int.TryParse(Console.ReadLine(), out int cyfra1) && cyfra1 > 0 && cyfra1 < 11) return cyfra1;
+            System.Console.WriteLine("Nieprawidłowa liczba");
+        }
+    }
+
+    static int[] UtworzTabliceLiczb(int n)
+    {
+        int[] T1 = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            T1[i] = dajcyfre();
+        }
+        return T1;
+    }
+
+    static int ObliczPole(int a1) {return a1*a1;}
+    static int ObliczObwod(int a1) {return (4 * a1);}
+    
+    static void wyswietlTAB(int[] T1)
+    {
+        System.Console.WriteLine("Oto twoja tablica:");
+        foreach(var i in T1) System.Console.WriteLine(i);
+    }
+    static void pokazelement()
+    {
+        System.Console.WriteLine("jaki element chcesz zobaczyć?");
+        int n;
+        System.Console.WriteLine("Podaj liczbę z zakresu 1-5:");
+        while(true)
+        {
+            if(int.TryParse(Console.ReadLine(), out int cyfra1) && cyfra1 > 0 && cyfra1 < 6) n = cyfra1;
+            System.Console.WriteLine("Nieprawidłowa liczba");
+        }
+        System.Console.WriteLine("Pole kwadratu utworzonego przy użyciu liczby " + n + " z twojej tablicy to: " + pola[n] + "cm\u00B2");
+        System.Console.WriteLine("Obwód kwadratu utworzonego przy użyciu liczby " + n + " z twojej tablicy to: " + obwody[n] + "cm");
+    }
+}
